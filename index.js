a776071e522335108ff8779a8e66c158dce92765
carlosmagno02 <carlosmabr44@gmail.com>
2025-04-05 21:18:38 -0300

    feat: add route to get a tesk by id

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ app.get("/tasks", async (req, res) => {
     }
 })
 
+app.get("/task/:id", async (req,res)=>{
+    try{
+        const taskId = req.params.id
+
+        const task = await TaskModel.findById(taskId)
+
+        if(!task){
+            return res.status(404).send("Task not found")
+        }
+
+        res.status(200).send(task)
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+})
+
 app.post("/task", async (req,res) =>{
     try{
         const newTask = new TaskModel(req.body);
@@ -37,7 +53,7 @@ app.delete("/task/:id", async (req,res)=>{
         const taskToDelete = await TaskModel.findById(taskId)
 
         if(!taskToDelete){
-            return res.status(500).send("Task not found")
+            return res.status(404).send("Task not found")
         }
 
         const deletedTask = await TaskModel.findByIdAndDelete(taskId)
